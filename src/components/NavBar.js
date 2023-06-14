eec0d74800642873e3394bddd66928dda8a6f89b
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-14 16:32:47 +0200

    create page and css finish

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { HashLink } from "react-router-hash-link";
-
+import logo from "../style/img/Fried-chicken-logo-template-on-transparent-background-PNG.png";
+import navIcon1 from "../style/img/nav-icon1.svg";
+import navIcon2 from "../style/img/nav-icon2.svg";
+import navIcon3 from "../style/img/nav-icon3.svg";
 import { BrowserRouter as Router } from "react-router-dom";
 
 export const NavBar = () => {
-  const [activeLink, setActiveLink] = useState("Home");
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -30,7 +33,9 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/"></Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="logo" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
@@ -41,7 +46,7 @@ export const NavBar = () => {
                 className={
                   activeLink === "home" ? "active navbar-link" : "navbar-link"
                 }
-                onClick={() => onUpdateActiveLink("Home")}>
+                onClick={() => onUpdateActiveLink("home")}>
                 Home
               </Nav.Link>
               <Nav.Link
@@ -49,7 +54,7 @@ export const NavBar = () => {
                 className={
                   activeLink === "skills" ? "active navbar-link" : "navbar-link"
                 }
-                onClick={() => onUpdateActiveLink("Skills")}>
+                onClick={() => onUpdateActiveLink("skills")}>
                 Skills
               </Nav.Link>
               <Nav.Link
@@ -59,12 +64,23 @@ export const NavBar = () => {
                     ? "active navbar-link"
                     : "navbar-link"
                 }
-                onClick={() => onUpdateActiveLink("Projects")}>
+                onClick={() => onUpdateActiveLink("projects")}>
                 Projects
               </Nav.Link>
             </Nav>
             <span className="navbar-text">
-              <div className="social-icon"></div>
+              <div className="social-icon">
+                {" "}
+                <a href="https://www.linkedin.com/in/paolo-angelani-b73131268/">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="#">
+                  <img src={navIcon2} alt="" />
+                </a>
+                <a href="#">
+                  <img src={navIcon3} alt="" />
+                </a>
+              </div>
               <HashLink to="#connect">
                 <button className="vvd">
                   <span>Let’s Connect</span>
