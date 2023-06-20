43dd1aeb3d7cb66e8f8b2abde41119a1a9a34caa
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-20 17:15:53 +0200

    bug fixed

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,9 @@ export const ProjectCard = ({ title, description, image }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <a href="https://github.com/paoloangelani?tab=repositories">
+        <a
+          className="mistery"
+          href="https://github.com/paoloangelani?tab=repositories">
           <img src={image} alt="project" />
           <div className="proj-txtx">
             <h4>{title}</h4>
