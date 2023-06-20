4ca98e78702ad62f9f9977451709ddb65a076de4
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-20 11:25:56 +0200

    add href on project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,11 +4,13 @@ export const ProjectCard = ({ title, description, image }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={image} alt="project" />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
+        <a href="https://github.com/paoloangelani?tab=repositories">
+          <img src={image} alt="project" />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
+        </a>
       </div>
     </Col>
   );
